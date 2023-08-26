5ec31ec4b5c9cfe8c5079d2bad797d9beae4c5f7
Eddie Cheng <schaffenneu47@gmail.com>
2023-08-26 20:32:47 +0800

    use arc and mutex to let workers share ownership of receiver safely

--- web-server/src/lib.rs
+++ web-server/src/lib.rs
@@ -1,7 +1,10 @@
-use std::thread;
+use std::{sync::{mpsc, Arc, Mutex}, thread};
+
+struct Job;
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
 
 impl ThreadPool {
@@ -16,14 +19,18 @@ impl ThreadPool {
         // size should be greater than 0
         assert!(size > 0);
 
+        let (sender, receiver) = mpsc::channel();
+
+        let thread_receiver = Arc::new(Mutex::new(receiver));
+
         let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
             // create some threads and store them in the vector
-            workers.push(Worker::new(id));
+            workers.push(Worker::new(id, Arc::clone(&thread_receiver)));
         }
 
-        ThreadPool { workers }
+        ThreadPool { workers, sender }
     }
 
     pub fn execute<F>(&self, f: F)
@@ -35,14 +42,16 @@ impl ThreadPool {
 
 // we want to create the threads and have them "wait for" code that we’ll send later. 
 // The standard library’s implementation of threads doesn’t include any way to do that
-pub struct Worker {
+struct Worker {
     id: usize,
     thread: thread::JoinHandle<()>,
 }
 
 impl Worker {
-    pub fn new(id: usize) -> Worker {
-        let thread = thread::spawn(|| {});
+    pub fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(|| {
+            receiver;
+        });
 
         Worker {id , thread}
     }
